622869388d8decd0750fa4580945f2fc984c01ad
Breno Rios <rbrenorios@gmail.com>
2019-07-25 17:33:03 -0300

    Authentication done; Project and Tasks models created

---
+++ bootcamp/gonode/goNodeAdonis/app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,70 @@
+'use strict'
+const User = use('App/Models/User')
+const crypto = require('crypto')
+const Mail = use('Mail')
+const moment = require('moment')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password', 'emails.forgot_password-text'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('rbrenorios@gmail.com', 'Breno Rios')
+            .subject('Password recovery')
+        }
+      )
+
+      console.log('After user save await')
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Something went wrong: check your email' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'Your recovery token expired' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({
+        error: {
+          message: 'Something went wrong while reseting your password'
+        }
+      })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
---
+++ bootcamp/gonode/goNodeAdonis/start/routes.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
+
+// User must send a id token to access these routes
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+  Route.resource('projects', 'ProjectController').apiOnly()
+}).middleware(['auth'])
+
+Route.get('files/:id', 'FileController.show')
